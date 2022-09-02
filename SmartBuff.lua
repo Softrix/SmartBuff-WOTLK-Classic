12c31804a4419cdb34b5595f94fc42a9ee3a81f8
Michael (a.k.a. Codermik) <Softrix@users.noreply.github.com>
2022-09-02 09:52:30 +0100

    Debugging code left on in r28
    Turned off some debugging / version check code left on in r28 - oops!

--- SmartBuff.lua
+++ SmartBuff.lua
@@ -7,7 +7,7 @@
 -------------------------------------------------------------------------------
 
 SMARTBUFF_DATE			= "020922";
-SMARTBUFF_VERSION       = "r28."..SMARTBUFF_DATE;
+SMARTBUFF_VERSION       = "r29."..SMARTBUFF_DATE;
 SMARTBUFF_VERSIONMIN	= 11403;			-- min version
 SMARTBUFF_VERSIONNR     = 30400;			-- max version
 SMARTBUFF_TITLE         = "SmartBuff";
@@ -20,10 +20,10 @@ SMARTBUFF_OPTIONS_TITLE = SMARTBUFF_VERS_TITLE;
 local addonName = ...
 local SmartbuffPrefix = "Smartbuff";
 local SmartbuffSession = true;
-local SmartbuffVerCheck = true;		-- for my use when checking guild users/testers versions  :)
+local SmartbuffVerCheck = false;		-- for my use when checking guild users/testers versions  :)
 local wowVersionString, wowBuild, _, wowTOC = GetBuildInfo();
 local isWOTLKC = (_G.WOW_PROJECT_ID == 5 and wowTOC >= 30000);
-local SmartbuffRevision = 28;
+local SmartbuffRevision = 29;
 local SmartbuffVerNotifyList = {}
 
 local LCD = LibStub and LibStub("LibClassicDurations", true)
